0ce7023ff9e00575c5020a60f2fa1eda60a04488
Ivan <ivan.burtak.23@pnu.edu.ua>
2026-02-25 20:52:30 +0100

    Use hash history and set Vite base for GitHub Pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Events from '../pages/Events.vue'
 import Members from '../pages/Members.vue'
@@ -15,5 +15,5 @@ const routes = [
     { path: '/apply', component: Apply },
 ]
 
-const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes })
+const router = createRouter({ history: createWebHashHistory(), routes })
 export default router
