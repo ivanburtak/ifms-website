b98d1c8d8a4e031fbe060cee67950cf513d8e51a
Ivan <ivan.burtak.23@pnu.edu.ua>
2026-03-08 16:40:57 +0100

    Add collectstatic

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,9 +11,13 @@ if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
     User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
 "
 
-echo "Starting server..."
 if [ "$DJANGO_DEBUG" = "1" ]; then
+    echo "Starting server..."
     python manage.py runserver 0.0.0.0:8000
 else
+    echo "Collecting static files..."
+    python manage.py collectstatic --noinput
+
+    echo "Starting server..."
     gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 4
 fi
